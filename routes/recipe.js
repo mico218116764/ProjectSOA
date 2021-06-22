a099320c1ef938443624a0e0d9b158605204cd9a
mico218116764 <72438982+mico218116764@users.noreply.github.com>
2021-06-22 16:55:37 +0700

    Update recipe.js

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,7 +1,48 @@
 const exp= require('express');
 const db = require('../database');
 const route = exp.Router();
+const multer = require('multer');
 
+const storage = multer.diskStorage({
+    destination:function(req,file,callback){
+        callback(null,'./uploads');
+    },
+    filename:async function(req,file,callback){
+        const extension = file.originalname.split('.')[file.originalname.split('.').length-1];
+        let conn = await db.getConn()
+        let select = await db.executeQuery(conn,`select * from recipe`)
+        let id = ''
+        if(select.length<10){
+            id ='RE00'+ (select.length+1)
+        }else if(select.length < 100){
+            id ='RE0'+(select.length+1)
+        }else{
+            id ='RE'+(select.length+1)
+        }
+
+
+        const filename =  id;
+        callback(null,(filename+'.'+extension));
+    }
+});
+
+function checkFileType(file,cb){
+    const filetypes = /jpeg|jpg|png|gif/;
+    const extname = filetypes.test(file.originalname.split('.')[file.originalname.split('.').length-1]);
+    const mimetype = filetypes.test(file.mimetype);
+    if(mimetype && extname){
+        return cb(null,true);
+    }else{
+        cb(error = 'Error : Image Only!');
+    }
+}
+
+const upload = multer({
+    storage:storage,
+    fileFilter: function(req,file,cb){
+        checkFileType(file,cb);
+    }
+});
 
 
 route.get('/all', async function (req,res){
@@ -16,7 +57,7 @@ route.get('/all', async function (req,res){
     }
 })
 
-route.post('/new', async function (req,res){
+route.post('/new', upload.single('foto_recipe'),async function (req,res){
     let conn = await db.getConn()
     let api = req.body.api_key
     let email = req.body.email
@@ -24,6 +65,7 @@ route.post('/new', async function (req,res){
     let tipe = req.body.tipe_diet
     let checkEmail = await db.executeQuery(conn,`select * from user where email = '${email}'`)
     let checkAPI = await db.executeQuery(conn, `select * from user where api_key = '${api}'`)
+    let dir = './uploads/'+req.file.filename;
     if(checkEmail[0] == null){
         res.status(404).send({msg:"Email user tidak ada"})
     }
@@ -33,24 +75,73 @@ route.post('/new', async function (req,res){
         let select = await db.executeQuery(conn,`select * from recipe`)
         let id = ''
         if(select.length<10){
-            id ='RE00'+select.length+1
+            id ='RE00'+ (select.length+1)
         }else if(select.length < 100){
-            id ='RE0'+select.length+1
+            id ='RE0'+(select.length+1)
         }else{
-            id ='RE'+select.length+1
+            id ='RE'+(select.length+1)
         }
         
         let q = await db.executeQuery(conn, `
-            INSERT INTO recipe VALUES ('${id}', '${email}', '${nama}',${tipe}, null)
+            INSERT INTO recipe VALUES ('${id}', '${email}', '${nama}',0,${tipe}, '${dir}')
         `);
-
         const hasil = {
-            email : email,
-            nama : nama,
-            api_key : api
+            id: id,
+            email: email,
+            nama: nama,
+            kalori: 0,
+            tipe: tipe,
         };
 
-        res.status(200).send({id})
+        res.status(200).send({hasil})
+    }
+})
+route.post('/add_bahan', upload.single('foto_recipe'),async function (req,res){
+    let conn = await db.getConn()
+    let api = req.body.api_key
+    let id_recipe = req.body.id_recipe
+    let jumlah = req.body.jumlah
+    let nama = req.body.nama_bahan
+    let satuan = req.body.satuan
+    let checkAPI = await db.executeQuery(conn, `select * from user where api_key = '${api}'`)
+    let checkRecipe = await db.executeQuery(conn, `select * from recipe where id_recipe = '${id_recipe}'`)
+    // return res.send(resultGet.data['calories']+"")
+    if(checkRecipe[0] == null){
+        res.status(404).send({msg:"id_recipe tidak ada"})
+    }
+    if(checkAPI[0] == null){
+        res.status(404).send({msg:"API tidak valid"})
+    }else{
+        let select = await db.executeQuery(conn,`select * from bahan`)
+        let id = ''
+        if(select.length<10){
+            id ='BN00'+ (select.length+1)
+        }else if(select.length < 100){
+            id ='BN0'+(select.length+1)
+        }else{
+            id ='BN'+(select.length+1)
+        }
+        
+        let q = await db.executeQuery(conn, `
+            INSERT INTO bahan VALUES ('${id}','${id_recipe}', '${nama}', '${jumlah}',${satuan})
+        `);
+        
+        let querySearch = 'https://api.edamam.com/api/nutrition-data?app_id=fcf84afe&app_key=cb2c4e1180b131a4b79eb3bb0fa9caf5%09&nutrition-type=logging&ingr='+nama
+        const axios = require("axios")
+        let resultGet = await axios.get(querySearch)
+        let total_kal = checkRecipe[0].kalori + resultGet.data['calories']
+        let update = await db.executeQuery(conn,`UPDATE recipe SET kalori = ${total_kal} where id_recipe = '${id_recipe}'`)
+        conn.release()
+        const hasil = {
+            id: id,
+            nama: nama,
+            jumlah: jumlah,
+            satuan: satuan,
+            "======Re":"sep======",
+            nama_recipe: checkRecipe[0].nama_recipe,
+            kalori: total_kal
+        };
+        res.status(200).send({hasil})
     }
 })
 
